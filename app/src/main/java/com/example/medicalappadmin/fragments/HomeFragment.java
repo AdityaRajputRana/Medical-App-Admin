4f133521792af8054186485a76425ec2c576b69d
Aditya Sood <adityasood314@gmail.com>
2023-12-23 16:11:57 +0530

    Minor changes in home fragment.

--- app/src/main/java/com/example/medicalappadmin/fragments/HomeFragment.java
+++ app/src/main/java/com/example/medicalappadmin/fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.medicalappadmin.fragments;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -14,14 +16,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.medicalappadmin.ActivityCaseHistory;
+import com.example.medicalappadmin.Models.User;
 import com.example.medicalappadmin.PatientHistoryActivity;
 import com.example.medicalappadmin.PenDriver.LiveData.PenStatusLiveData;
 import com.example.medicalappadmin.PrescriptionActivity;
 import com.example.medicalappadmin.R;
+import com.example.medicalappadmin.Transformations.CircleTransformation;
 import com.example.medicalappadmin.databinding.ActivityPatientHistoryBinding;
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class HomeFragment extends Fragment {
@@ -31,10 +42,14 @@ public class HomeFragment extends Fragment {
     }
 
     private CallBacksListener listener;
-    LinearLayout btnAddNewPatient;
-    LinearLayout btnCaseHistory;
-    LinearLayout btnPatientHistory;
-    ImageButton penButton;
+    private LinearLayout btnAddNewPatient;
+    private LinearLayout btnCaseHistory;
+    private LinearLayout btnPatientHistory;
+    private ImageButton penButton;
+    private TextView tvGreeting;
+    private TextView tvDoctorName;
+    private ImageView ivProfilePic;
+    private User user;
 
 
 
@@ -56,6 +71,9 @@ public class HomeFragment extends Fragment {
         btnCaseHistory  = view.findViewById(R.id.btnCaseHistory);
         btnPatientHistory  = view.findViewById(R.id.btnPatientHistory);
         penButton = view.findViewById(R.id.penStatusBtn);
+        tvDoctorName = view.findViewById(R.id.tvDoctorName);
+        tvGreeting = view.findViewById(R.id.tvGreeting);
+        ivProfilePic = view.findViewById(R.id.ivProfilePic);
 
         return  view;
     }
@@ -99,7 +117,41 @@ public class HomeFragment extends Fragment {
                     int colorCodeID = isConnected?R.color.colorActiveGreen:R.color.colorInactiveGray;
                     penButton.setColorFilter(getActivity().getColor(colorCodeID));
                 });
+        loadData();
+
 
 
     }
+
+
+    private void loadData() {
+        user = new Gson().fromJson(
+                getActivity().getSharedPreferences("MY_PREF", MODE_PRIVATE).getString("MY_USER", "{}"),
+                User.class
+        );
+        if(user != null){
+            tvDoctorName.setText("Dr. " + user.getName());
+            if(!user.getDisplayPicture().equals("") || user.getDisplayPicture() != null){
+                Picasso.get().load(user.getDisplayPicture()).transform(new CircleTransformation()).into(ivProfilePic);
+            }
+        } else {
+            tvDoctorName.setVisibility(View.GONE);
+        }
+        Calendar calendar = Calendar.getInstance();
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+        String greeting = null;
+        if(hours>=1 && hours<=12){
+            greeting = "Good Morning,";
+        } else if(hours>=12 && hours<=16){
+            greeting = "Good Afternoon,";
+        } else if(hours>=16 && hours<=21){
+            greeting = "Good Evening,";
+        } else if(hours>=21 && hours<=24){
+            greeting = "Good Night,";
+        } else {
+            greeting = "Hello";
+        }
+        tvGreeting.setText(greeting);
+    }
+
 }
